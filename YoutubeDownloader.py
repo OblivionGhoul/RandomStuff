e6b2a86c0130c7c71e0d704b1acf0d26b4a866a8
Minh DInh <minhthedinh@users.noreply.github.com>
2020-12-23 12:33:15 -0800

    Simple Youtube Downloader

---
+++ YoutubeDownloader.py
@@ -0,0 +1,44 @@
+import pytube
+
+print("Welcome to Oblivion Ghoul's Youtube Video Downloader! Please choose an option below.")
+print(
+"""
+1. Download 1 Youtube Video
+2. Download Multiple Youtube Videos
+"""
+)
+try:
+    option = int(input())
+except: 
+    print('Not an int!')
+else: 
+    if (option == 1):
+        try:
+            url = input("Enter the URL: ")
+            v = pytube.YouTube(url)
+            stream = v.streams.get_by_itag(22)
+            print("Downloading video...")
+            stream.download()
+            print("Download Complete!")
+        except:
+            print('Please Enter a Valid URL')
+    elif (option == 2):
+        try:
+            videoList = []
+            print("Enter the video URL. Enter 'STOP' to stop the process.")
+            while True:
+                url = input("")
+                if url == 'STOP':
+                    break
+                videoList.append(url)
+
+            for x, video in enumerate(videoList):
+                v = pytube.YouTube(video)
+                stream = v.streams.get_by_itag(22)
+                print(f"Downloading video {x}...")
+                stream.download()
+                print("Download Complete!")
+        except:
+            print('Please Enter Valid URLs')
+    else:
+        print("Please enter a valid option.")
